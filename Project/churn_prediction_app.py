de4944a1c8af74bbf5d14073a4133653305be369
Arti Tatyasaheb Sonawane <161977226+arti3104@users.noreply.github.com>
2025-07-21 00:16:52 +0530

    Update churn_prediction_app.py

--- Project/churn_prediction_app.py
+++ Project/churn_prediction_app.py
@@ -5,6 +5,10 @@ import joblib
 import shap
 import matplotlib.pyplot as plt
 import plotly.express as px
+import os, streamlit as st
+
+st.write("📂 Current working directory:", os.getcwd())
+st.write("🗂️ Files in this directory:", os.listdir())
 
 # Page config
 st.set_page_config(page_title="Customer Churn Predictor", layout="wide")
