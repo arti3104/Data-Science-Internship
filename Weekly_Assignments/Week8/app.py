a74ccf072520aabc74e4a2218e817aa3372f6cef
Arti Tatyasaheb Sonawane <161977226+arti3104@users.noreply.github.com>
2025-07-28 00:44:30 +0530

    Update app.py

--- Weekly_Assignments/Week8/app.py
+++ Weekly_Assignments/Week8/app.py
@@ -4,7 +4,7 @@ from rag_pipeline import RAGChatbot
 st.set_page_config(page_title="Loan Q&A Chatbot", layout="centered")
 st.title("📊 Loan Approval RAG Chatbot")
 
-bot = RAGChatbot("data/Training Dataset.csv")
+bot = RAGChatbot("Weekly_Assignments/Week8/data/Training Dataset.csv")
 
 question = st.text_input("Ask a question about the loan data:")
 if st.button("Get Answer") and question:
